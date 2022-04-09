d4125a1997e6143ee0d05036a42a00e0a2953524
TylerLin <Raphaelywchang@gmail.com>
2022-04-09 10:52:17 -0400

    Finished the prove yourself and stacks

--- Stack-TL/Assets/GameController.cs
+++ Stack-TL/Assets/GameController.cs
@@ -47,20 +47,22 @@ public class GameController : MonoBehaviour
         //Variable pos1 equals last cube position
         var pos1 = lastCube.transform.position + Vector3.up * 10f;
         //Variable pos2 equals to the pos1 plus any level by number of 2
-        var pos2 = pos1 + ((Level % 2 == 0) ? Vector3.left : Vector3.forward) * 120;
+        //var pos2 = pos1 + ((Level % 2 == 0) ? Vector3.left : Vector3.forward) * 120;
+        var pos2 = pos1 + (Vector3.left + Vector3.forward) * 120;
         //if the level is by the number of two
-        
+        //    var pos3 = pos1 - ((Level % 2 == 0) ? Vector3.left : Vector3.forward) * 120;
+        var pos3 = pos1 - (Vector3.left + Vector3.forward) * 120;
         if (Level % 2 == 0)
         {
             //Current position of the current cube based of the 3 axis of
             //pos2, pos1, and time
-            currentCube.transform.position = Vector3.Lerp(pos2, pos1, time);
+            currentCube.transform.position = Vector3.Lerp(pos2, pos3, time);
         }
         else
         {
             //Current position of the current cube based of the 3 axis of
             //pos1, pos2, and time
-            currentCube.transform.position = Vector3.Lerp(pos1, pos2, time);
+            currentCube.transform.position = Vector3.Lerp(pos3, pos2, time);
         }
         //If left button is clicked 
         //Debug.Log(currentCube.transform.position);
