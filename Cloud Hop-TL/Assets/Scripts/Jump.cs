9b326d67f08e291b9fca86f47db0392a2d685f4c
rebecca <rebeccaymchang@gmail.com>
2022-05-05 19:54:32 -0400

    done some steps

---
+++ Cloud Hop-TL/Assets/Scripts/Jump.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+    Rigidbody2D rigidbody;
+
+    float jumpForce = 15;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            rigidbody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+    }
+}
