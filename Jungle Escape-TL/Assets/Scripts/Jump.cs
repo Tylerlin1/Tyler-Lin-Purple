8c188078aa665c25ef5b95cc54223e3f1ac905ba
Tylerlin1 <98924260+Tylerlin1@users.noreply.github.com>
2022-05-12 19:15:05 -0400

    Finished cloud hop and starting jungle escape

---
+++ Jungle Escape-TL/Assets/Scripts/Jump.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+    Rigidbody rigidbody;
+
+    float jumpForce = 5.7f;
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if(rigidbody.velocity.y > -.01 && rigidbody.velocity.y < .01)
+        {
+
+        }
+        if(Input.GetButtonDown("Jump")){
+            rigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+}
+
