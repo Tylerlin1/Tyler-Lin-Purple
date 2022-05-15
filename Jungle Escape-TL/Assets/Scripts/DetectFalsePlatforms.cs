93673a4553a1121b601aab79a1fcfe2c379e203d
Raphael <Raphaelywchang@gmail.com>
2022-05-15 15:59:08 -0400

    Completed Jungle Escape and started jungle escape py

---
+++ Jungle Escape-TL/Assets/Scripts/DetectFalsePlatforms.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectFalsePlatforms : MonoBehaviour
+{
+    public bool hit;
+
+    void Update()
+    {
+
+    }
+}
