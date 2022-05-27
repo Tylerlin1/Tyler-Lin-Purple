c10ee7357a515cfde239d8438a3c017ffd36743b
Rexanna <rexanna612@gmail.com>
2022-05-27 18:58:23 -0400

    Finished NinjaRunPY

--- NinjaRunPY-TL/Assets/Scripts/Pickup.cs
+++ NinjaRunPY-TL/Assets/Scripts/Pickup.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class Pickup : MonoBehaviour
 {
+
+    public GameObject kk;
     // Start is called before the first frame update
     void Start()
     {
-        
+        //kk = GameObject.Find("door");
     }
 
     // Update is called once per frame
@@ -18,6 +20,11 @@ public class Pickup : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.gameObject.CompareTag("coin"))
+        {
+            Destroy(other.gameObject);
+
+            Destroy(GameObject.Find("door"));
+        }
     }
 }
