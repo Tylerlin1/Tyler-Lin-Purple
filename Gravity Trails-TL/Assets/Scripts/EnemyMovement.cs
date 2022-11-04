4b225ad1ce640bd400dbb5c1176cd826f02760f0
Cleo <cleotran05@gmail.com>
2022-11-03 20:00:53 -0400

    Worked on gravity trails
    This reverts commit 4c340358d05209708c973fa1238dc04b49050e58.

--- Gravity Trails-TL/Assets/Scripts/EnemyMovement.cs
+++ Gravity Trails-TL/Assets/Scripts/EnemyMovement.cs
@@ -1,13 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class EnemyMovement : MonoBehaviour
 {
     public float yForce;
     public float xForce;
     private Rigidbody2D enemyRigidbody;
+    public int maximumXPosition;
+    public int minimumXPosition;
     // Start is called before the first frame update
     void Start()
     {
@@ -24,6 +25,16 @@ public class EnemyMovement : MonoBehaviour
     {
         if (collision.gameObject.tag == "Ground")
         {
+            Vector2 jumpForce = new Vector2(xForce, yForce);
+            enemyRigidbody.AddForce(jumpForce);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            Debug.Log("h");
             Vector2 jumpForce = new Vector2(xForce, yForce);
             enemyRigidbody.AddForce(jumpForce);
         }
--- Gravity Trails-TL/Assets/Scripts/GravityFlip.cs
+++ Gravity Trails-TL/Assets/Scripts/GravityFlip.cs
@@ -16,7 +16,7 @@ public class GravityFlip : MonoBehaviour
     {
         if (Input.GetButtonDown("Jump"))
         {
-            avatarRigidbody.gravityScale += -1;
+            avatarRigidbody.gravityScale *= -1;
             Vector3 newDirection = transform.localScale;
             newDirection.y *= -1;
             transform.localScale = newDirection;
