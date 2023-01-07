fd7c82127acaeabbde30904c709405e68a289e53
rebecca <leahmelanie999@gmail.com>
2023-01-07 11:58:03 -0500

    Worked on Codey Raceway

---
+++ Codey Raceway-TL/Assets/Scripts/checkpointCollision.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class checkpointCollision : MonoBehaviour
+{
+
+    public bool didCollide;
+    public CheckpointCounter count;
+    // Start is called before the first frame update
+    void Start()
+    {
+        didCollide = false;
+    }
+
+    // Update is called once per frame
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player" && didCollide == false)
+        {
+            didCollide = true;
+            count.triggeredCheckpoints++;
+        }
+    }
+}
--- Codey Raceway-TL/Assets/TriggerFinishLine.cs
+++ Codey Raceway-TL/Assets/TriggerFinishLine.cs
@@ -4,23 +4,21 @@ using UnityEngine;
 
 public class TriggerFinishLine : MonoBehaviour
 {
-
+    public CheckpointCounter checkpointTracker;
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.tag == "Player")
         {
-            print("You Win!");
+            if (checkpointTracker.triggeredCheckpoints == checkpointTracker.numberOfCheckpoints)
+            {
+                print("You Win!");
+            }
+            else
+            {
+                print("Cheater!");
+            }
         }
     }
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
